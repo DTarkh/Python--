337ea6e0a6479a4e14485240be5b6226c347129e
David Tarkhnishvili <datotarkh@gmail.com>
2024-08-15 01:47:12 +0400

    finished second exercise

--- Python-16/app.py
+++ Python-16/app.py
@@ -32,3 +32,29 @@ print(get.get_money(500))
 # student_id და courses(კურსების სია, რომელშიც სტუდენტი არის ჩარიცხული).
 # აღწერეთ სტუდენტის ინფორმაციის და კურსების ჩვენების მეთოდები. შექმენით
 # რამდენიმე ობიექტი და დაარეგისტრირეთ სხვადასხვა კურსებზე.
+
+
+class Student:
+
+
+    def __init__(self, name, student_id, courses):
+        self.name = name
+        self.student_id = student_id
+        self.courses = courses
+    
+
+    def get_info(self):
+        print( f"Name: {self.name}, Student ID: {self.student_id}, Courses: {self.courses}")
+
+    def register_on_math(self):
+        print("You have successfully registered on Math course")
+
+    def register_on_English(self):
+        print("You have successfully registered on English course")
+
+
+student1 = Student("David", "334AA", "Math")
+student2 = Student("Diliya", "556BB", "How to get Rich")
+student1.get_info()
+student2.get_info()
+student1.register_on_math()
