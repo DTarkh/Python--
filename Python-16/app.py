dcc7832fd24d7c65f68f85e826b153ef55e6e25d
David Tarkhnishvili <datotarkh@gmail.com>
2024-08-15 22:10:12 +0400

    fixed some issues

--- Python-16/app.py
+++ Python-16/app.py
@@ -7,9 +7,11 @@
 
 
 class BankAccount:
-    account_number = "TB3444A422B00006"
-    account_holder = "John Doe"
-    balance = 576
+
+    def __init__(self, account_number, account_holder, balance):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = balance
 
     def add_money(self, money):
         self.balance += money
@@ -20,11 +22,14 @@ class BankAccount:
         return self.balance
 
 
-add = BankAccount()
-print(add.add_money(200))
+client = BankAccount("TB3444A422B00006", "John Doe", 576)
+client2 = BankAccount("TB3444A422B00006", "David Tarkhnishvili", 1420)
 
-get = BankAccount()
-print(get.get_money(500))
+print(f"{client.account_holder} has {client.balance} GEL on balance")
+print(f"He is adding 200 that results to have {client.add_money(200)}")
+print(f"He is withdrawing 300 that results to have {client.get_money(300)}")
+print(f"{client2.account_holder} has {client2.balance} GEL on balance")
+print(f"He is withdrawing 550 that results to have {client2.get_money(500)}")
 
 # დავალება 2.
 
@@ -36,25 +41,28 @@ print(get.get_money(500))
 
 class Student:
 
-
     def __init__(self, name, student_id, courses):
         self.name = name
         self.student_id = student_id
         self.courses = courses
-    
 
     def get_info(self):
-        print( f"Name: {self.name}, Student ID: {self.student_id}, Courses: {self.courses}")
+        print(f"Name: {self.name}, Student ID: {self.student_id}, Courses: {self.courses}")
 
     def register_on_math(self):
-        print("You have successfully registered on Math course")
+        print("You have successfully registered on \"Math\" course")
 
     def register_on_English(self):
-        print("You have successfully registered on English course")
+        print("You have successfully registered on \"English\" course")
+
+    def register_on_HowtogetRich(self):
+        print("You have successfully registered on \"How to get Rich course\"")
 
 
-student1 = Student("David", "334AA", "Math")
-student2 = Student("Diliya", "556BB", "How to get Rich")
+student1 = Student("David", "334AA", ["Math", "English"])
+student2 = Student("Diliya", "556BB", ["How to get Rich", "French", "English"])
 student1.get_info()
 student2.get_info()
 student1.register_on_math()
+student2.register_on_HowtogetRich()
+student1.register_on_English()
