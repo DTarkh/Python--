120acabad0a9bdce81dfe7c3606e7690eacc5df2
David Tarkhnishvili <datotarkh@gmail.com>
2024-09-10 01:37:10 +0400

    22

--- Python-22/app.py
+++ Python-22/app.py
@@ -1,11 +1,11 @@
-# საშინაო დავალება - შექმენით სტუდენტის ობიექტი 
-# სახელი, გვარი, დაბადების წელი და ქულა. 
-# შექმენით ამ კლასის სამი სხვადასხვა ობიექტი. 
+# საშინაო დავალება - შექმენით სტუდენტის ობიექტი
+# სახელი, გვარი, დაბადების წელი და ქულა.
+# შექმენით ამ კლასის სამი სხვადასხვა ობიექტი.
 # დაწერეთ ფუნქცია რომელიც არგუმენტად მიიღებს ობიექტს.
 # ჯერ სცდის JSON-თ ჩაწერას თუ პროცესი წარუმატებელი აღმოჩნდა,
-# სცდის pickle-ით ჩაწერას, თუ ფიქლიც წარუმატებელი აღმოჩნდა 
+# სცდის pickle-ით ჩაწერას, თუ ფიქლიც წარუმატებელი აღმოჩნდა
 # შემდეგ სცდის Dill-ით ჩაწერას. თუ ვერცერთით ვერ ხდება
-# ჩაწერა დააბრუნოს პასუხი რომ ვერცერთ მეთოდი ვერ 
+# ჩაწერა დააბრუნოს პასუხი რომ ვერცერთ მეთოდი ვერ
 # ასერიალიზებს გადაცემულ ობიექტს
 
 
@@ -33,18 +33,24 @@ import dill
 
 
 def serialize_data(obj):
+    try:
+        with open("file.json", "w") as f:
+            json.dump(obj, f)
+        print("successfully serialized")
+    except (TypeError, json.JSONDecodeError )as error:
+        print(f"JSON serialization failed: {error}")
+        try:
+            with open("file.pkl", "wb") as f:
+                pickle.dump(obj, f)
+        except pickle.PicklingError as error:
+            print(f"Pickle serialization failed: {error}")
+            try:
+                with open("file.dill", "wb") as f:
+                    dill.dump(obj, f)
+            except dill.PicklingError as error:
+                print(f"Dill serialization failed: {error}")
+                return "Unable to serialize the object with any method"
 
-try: 
-    with open("file.json" "w") as f:
-        json.dump(obj, f)
 
-    with open("file.pckl" "wb") as f:
-        pickle.dump(obj, f)
-
-    with open("file.dill" "wb") as f:
-        dill.dump(obj, f)
-except:
-    print("Failed to serialize data.")
-    return None
 
 print(serialize_data(student1))
