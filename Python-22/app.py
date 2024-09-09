9c4def5d9372567413616fbd8035d3c7de4232ec
David Tarkhnishvili <datotarkh@gmail.com>
2024-09-10 01:13:39 +0400

    ddd

--- Python-22/app.py
+++ Python-22/app.py
@@ -7,3 +7,44 @@
 # შემდეგ სცდის Dill-ით ჩაწერას. თუ ვერცერთით ვერ ხდება
 # ჩაწერა დააბრუნოს პასუხი რომ ვერცერთ მეთოდი ვერ 
 # ასერიალიზებს გადაცემულ ობიექტს
+
+
+
+class Student:
+    def __init__(self, name, surname, birth_year, grade):
+        self.name = name
+        self.surname = surname
+        self.birth_year = birth_year
+        self.grade = grade
+
+    
+
+student1 = Student("David", "Tarkhnishvili", 1992, 100)
+student2 = Student("Mary", "Popins", 1986, 87)
+student3 = Student("Elizabeth", "Queen", 1996, 93)
+
+
+
+import json
+import pickle
+import dill
+
+
+
+
+def serialize_data(obj):
+
+try: 
+    with open("file.json" "w") as f:
+        json.dump(obj, f)
+
+    with open("file.pckl" "wb") as f:
+        pickle.dump(obj, f)
+
+    with open("file.dill" "wb") as f:
+        dill.dump(obj, f)
+except:
+    print("Failed to serialize data.")
+    return None
+
+print(serialize_data(student1))
