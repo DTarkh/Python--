b3ab83178193454c9611530bf96c6a77374104a0
David Tarkhnishvili <datotarkh@gmail.com>
2024-09-20 01:20:12 +0400

    final

--- Python-26/app.py
+++ Python-26/app.py
@@ -8,3 +8,33 @@
 # მომხმარებლისთვის. პროგრამა დაასრულებს მუშაობას როდესაც ყველა
 # მომხმარებელი იყიდის ბილეთს.
 # პ.ს. გამოიყენეთ parking-ის ნაწილი რომელიც ლექციაზე გავაკეთეთ
+
+
+import threading
+import time
+
+
+
+
+semaphore = threading.Semaphore(5)
+
+
+def tickets_sell(customer):
+    print(f"customer: {customer} has started a session to buy a ticket")
+    print(f"customer: {customer} is in the session")
+    semaphore.acquire()
+    print(f"customer: {customer} has bought a ticket and left")
+
+    time.sleep(5)
+    semaphore.release()
+list_of_customers = ["customer1", "customer2", "customer3", "customer4", "customer5", "customer6", "customer7", "customer8", "customer9", "customer10"]
+threads_list = [threading.Thread(target=tickets_sell, args=(customer,)) for customer in list_of_customers]
+
+
+for thread in threads_list:
+    thread.start()
+
+for thread in threads_list:
+    thread.join()
+
+
