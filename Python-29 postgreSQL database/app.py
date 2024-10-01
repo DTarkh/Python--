2a0774b40ffefd7a1d7bcc7617848efaf6a01f67
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-01 08:39:48 +0400

    final

--- Python-29 postgreSQL database/app.py
+++ Python-29 postgreSQL database/app.py
@@ -9,22 +9,31 @@
 # •	Interest
 # •	Comission
 # •	Total
-#
+
 # 2. სვეტების მონაცემთა ტიპები განსაზღვრეთ ლოგიკურად
-#
+
 # 3. შეავსეთ ცხრილი ინფორმაციით 2-3 დეპოზიტორის შესახებ (სვეტები DevOpOwner, DateOfBirth, City, StreetName)
-#
+
 # 4. შეავსეთ ცხრილი ინფორმაციით 5-6 დეპოზიტორის შესახებ (სვეტები DepOwnerName, DateOfBirth, DepositAmount, Comission, Total)
-#
+
 # 5. გამოიტანეთ აბსოლუტურად ყველა დეპოზიტორი
-#
+
 # 6. გამოიტანეთ ყველა ის დეპოზიტორი, რომლის დეპოზიტიც არის 1500-ზე მეტი
-#
+
 # 7. გამოიტანეთ ყველა ის დეპოზიტორი, რომელიც ცხოვრობს თბილისში რუსთაველის ქუჩაზე
-#
+
 # 8. გამოიტანეთ ყველა ის დეპოზიტორი, რომელიც ცხოვრობს ბათუმში, გორგასალიზ ქუჩაზე, დეპოზიტი აქვთ 1000-ზე მეტი და 2000-ზე ნაკლები
-#
+
 # 9. გამოიტანეთ ყველა ის დეპოზიტორი რომლის სახელიც იწყება ასო “დ”-ზე
+
+
 #
 #
-#
+# select * from deposits
+# select * from deposits where depositamount > 1500
+# select * from deposits where city = 'Tbilisi' and streetname = 'Rustaveli'
+# select * from deposits where city = 'Batumi' and streetname = 'Gorgasali street' and depositamount > 1000 and depositamount < 2000
+# select * from deposits where depownername like 'C%'
+
+
+
